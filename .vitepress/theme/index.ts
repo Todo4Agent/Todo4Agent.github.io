b3a36516483d417466aa0be40677d3b9303335b1
BlazeSnow <git@blazesnow.org>
2026-09-21 10:40:00 +0800

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,11 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import './color.css'
-import DownloadLinks from '../custom/DownloadLinks.vue'
 
 export default {
-    extends: DefaultTheme,
-    enhanceApp({ app }) {
-        app.component('DownloadLinks', DownloadLinks)
-    },
+    extends: DefaultTheme
 } satisfies Theme
